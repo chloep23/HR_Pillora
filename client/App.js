b1557115e1a6d85f814567b4b0779b0f6f6c522a
chloep23 <chloesp23@gmail.com>
2024-09-21 15:28:15 -0500

    Test Git Connection

--- client/App.js
+++ client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start hello working on your app!</Text>
+      <Text>Pillora!!</Text>
       <StatusBar style="auto" />
     </View>
   );
