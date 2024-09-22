65b37898c3e81cd907fbaa08541fd8ba1e1351d5
Hosung Kim <hk63@rice.edu>
2024-09-22 02:50:59 -0500

    hi

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,13 @@ import Loginpage from  './screens/loginpage';
 import Pillpage from './screens/pillpage';
 import Chatpage from './screens/chatpage'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <Router>
       <App />
       <Routes>
@@ -21,6 +24,7 @@ root.render(
           <Route path='/chat' element={<Chatpage />}/>
       </Routes>
     </Router>
+    </Provider>
   </React.StrictMode>
 );
 
